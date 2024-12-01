12451526983de0943a2fd3b406884a25d87bbf4c
StackPointerr <krzysztofp730@outlook.com>
2024-12-01 15:46:23 +0100

    Dodano obsługe wyszukiwania przepisów

--- routes/api/recipe.js
+++ routes/api/recipe.js
@@ -1,11 +1,64 @@
 const express = require("express");
 const fs = require("fs");
+const Fuse = require("fuse.js");
 const router = express.Router();
 
 const Przepisy = require("../../models").przepis;
 const User = require("../../models").user;
 
 const loggedJson = require("../../middleware/index").loggedJson;
+
+router.get("/", loggedJson, async (req, res, next) => {
+    if (!req.query.nazwa) return next();
+
+    let przepisy = await Przepisy.find({}).populate(["autor", "kategoria"]);
+
+    if (req.query.kategoria) {
+        przepisy = przepisy.filter(
+            (przepis) => przepis.kategoria.nazwa == req.query.kategoria,
+        );
+    }
+
+    const fuse = new Fuse(przepisy, { keys: ["nazwa"] });
+    przepisy = fuse
+        .search(req.query.nazwa)
+        .map(({ item }) => item)
+        .map((przepis) => ({
+            id: przepis._id,
+            nazwa_przepisu: przepis.nazwa,
+            opis: przepis.opis,
+            opis_przygotowania: przepis.opis_przygotowania,
+            zdjecie: przepis.zdjecie,
+            kategoria: przepis.kategoria.nazwa,
+            data_utworzenia: przepis.data_utworzenia,
+            autor: przepis.autor.username,
+            czy_jest_autorem: przepis.autor._id.equals(req.user._id),
+        }));
+
+    res.send({ success: true, przepisy: przepisy });
+});
+
+router.get("/", loggedJson, async (req, res, next) => {
+    if (!req.query.kategoria) return next();
+
+    let przepisy = await Przepisy.find({}).populate(["autor", "kategoria"]);
+
+    przepisy = przepisy
+        .filter((przepis) => przepis.kategoria.nazwa == req.query.kategoria)
+        .map((przepis) => ({
+            id: przepis._id,
+            nazwa_przepisu: przepis.nazwa,
+            opis: przepis.opis,
+            opis_przygotowania: przepis.opis_przygotowania,
+            zdjecie: przepis.zdjecie,
+            kategoria: przepis.kategoria.nazwa,
+            data_utworzenia: przepis.data_utworzenia,
+            autor: przepis.autor.username,
+            czy_jest_autorem: przepis.autor._id.equals(req.user._id),
+        }));
+
+    res.json({ success: true, przepisy: przepisy });
+});
 
 router.get("/", loggedJson, async (req, res) => {
     let przepisy = await Przepisy.find({}).populate(["autor", "kategoria"]);
